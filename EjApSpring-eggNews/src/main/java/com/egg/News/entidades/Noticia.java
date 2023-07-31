b06a0a74cff1d6ef350f9ef298a9ebee892e92f0
marcos-git01 <marcoslefama02@gmail.com>
2023-07-31 17:32:26 -0300

    Commit 1: Inicio proyecto eggNews

---
+++ EjApSpring-eggNews/src/main/java/com/egg/News/entidades/Noticia.java
@@ -0,0 +1,48 @@
+
+package com.egg.News.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Noticia {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String titulo;
+    private String cuerpo;
+
+    public Noticia() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+    
+    
+    
+}
